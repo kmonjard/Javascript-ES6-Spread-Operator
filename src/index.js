449797171a9d1cb0965ae0d5bb7433e436f21e94
KMONJARD <62231623+kmonjard@users.noreply.github.com>
2021-05-16 10:50:46 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const citrus = ["lime", "Lemon", "Orange"];
+// const fruits = ["Apple", ...citrus, "Banana", "Watermelon"]
+
+// console.log(fruits);
+
+// const fullName = {
+//   fName: "James",
+//   lName: "Bond"
+// };
+
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   username: "jamesbond007"
+// };
+
+// console.log(user);
+
+// See Spread Syntax -- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+
+// See also JavaScript set object key by variable [duplicate] --
+// https://stackoverflow.com/questions/11508463/javascript-set-object-key-by-variable?noredirect=1&lq=1
